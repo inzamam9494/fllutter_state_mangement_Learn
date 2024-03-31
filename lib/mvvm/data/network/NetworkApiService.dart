b88596cc5857ac83abfb444fda1e000c5623704b
mohdi <mohdinzamam12125@gmail.com>
2024-03-31 23:12:27 +0530

    Day 9 MVVM implement

---
+++ lib/mvvm/data/network/NetworkApiService.dart
@@ -0,0 +1,49 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:state_management/mvvm/data/app_exception.dart';
+import 'package:state_management/mvvm/data/network/BaseApiServices.dart';
+
+class NetworkApiService extends BaseApiServices {
+  @override
+  Future getGetApiResponse(String url) async {
+    dynamic responseJson;
+    try {
+      final response = await http.get(Uri.parse(url)).timeout(const Duration(seconds: 10));
+      responseJson = returnResponse(response);
+    } on SocketException {
+      throw FetchDataException("No Internet Connection");
+    }
+    return responseJson;
+  }
+
+  @override
+  Future getPostApiResponse(String url, dynamic data) async {
+    dynamic responseJson;
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        body: data
+      ).timeout(const Duration(seconds: 10));
+      responseJson = returnResponse(response);
+    } on SocketException {
+      throw FetchDataException("No Internet Connection");
+    }
+    return responseJson;
+  }
+
+  dynamic returnResponse(http.Response response) {
+    switch (response.statusCode) {
+      case 200:
+        dynamic responseJson = jsonDecode(response.body);
+        return responseJson;
+      case 400:
+        throw BadRequestException(response.body.toString());
+      default:
+        throw FetchDataException(
+            'Error occurred while communication with server' +
+                'with status code' +
+                response.statusCode.toString());
+    }
+  }
+}
