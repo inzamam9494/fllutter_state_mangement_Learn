f828c74cf07ed836316d21fd67a6a230610fd258
mohdi <mohdinzamam12125@gmail.com>
2024-04-05 23:03:08 +0530

    Day 12 MVVM implement Alert Bars

--- lib/mvvm/view/login_screen.dart
+++ lib/mvvm/view/login_screen.dart
@@ -11,16 +11,63 @@ class LoginScreenMVVM extends StatefulWidget {
 }
 
 class _LoginScreenMVVMState extends State<LoginScreenMVVM> {
+
+  ValueNotifier<bool> _obsecurePassword = ValueNotifier<bool>(true);
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  final FocusNode emailFocusNode = FocusNode();
+  final FocusNode passwordFocusNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: InkWell(
-          onTap: (){
-            // Utils.toastMessage("error");
-            Utils.snackBar("No Internet Connection", context);
-          },
-          child: const Text('Click'),
+      appBar: AppBar(
+        title: const Center(child: Text("Login",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),),),
+        backgroundColor: Colors.blue.shade400,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              focusNode: emailFocusNode,
+              decoration: const InputDecoration(
+                hintText: "Email",
+                labelText: "Email",
+                prefixIcon: Icon(Icons.alternate_email)
+              ),
+              onFieldSubmitted: (value){
+               Utils.fieldFocusChange(context, emailFocusNode, passwordFocusNode);
+              },
+            ),
+            ValueListenableBuilder(
+                valueListenable: _obsecurePassword,
+                builder: (context, value, child){
+                  return TextFormField(
+                    controller: _passwordController,
+                    focusNode: passwordFocusNode,
+                    obscureText: _obsecurePassword.value,
+                    decoration: InputDecoration(
+                        hintText: "Password",
+                        labelText: "Password",
+                        prefixIcon: Icon(Icons.lock_outline),
+                        suffixIcon: InkWell(
+                          onTap: (){
+                            _obsecurePassword.value = !_obsecurePassword.value;
+                          },
+                            child: Icon(
+                                _obsecurePassword.value ?
+                                Icons.visibility_off_outlined: Icons.visibility_outlined))
+                    ),
+                  );
+                })
+
+          ],
         ),
       )
     );
