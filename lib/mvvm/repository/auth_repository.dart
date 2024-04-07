71232efa3787c15a73a02d954ef523cc1066692f
mohdi <mohdinzamam12125@gmail.com>
2024-04-07 23:28:37 +0530

    Day 12 MVVM implement Alert Bars

---
+++ lib/mvvm/repository/auth_repository.dart
@@ -0,0 +1,6 @@
+import 'package:state_management/mvvm/data/network/BaseApiServices.dart';
+import 'package:state_management/mvvm/data/network/NetworkApiService.dart';
+
+class AuthRepository{
+  BaseApiServices apiServices = NetworkApiService();
+}
